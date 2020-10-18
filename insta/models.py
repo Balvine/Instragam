d8b7f8556b3c563d04b693c17f54a0db9956aeb6
Balvine <balvy075@gmail.com>
2020-10-18 19:29:16 +0300

    created image model

--- insta/models.py
+++ insta/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
+from django.dispatch import receiver
+# from tinymce.models import HTMLField
+from django.db.models.signals import post_save
+# from friendship.models import Friend, Follow, Block
 
 
 
@@ -51,4 +54,91 @@ class Profile(models.Model):
     def get_profile_by_username(cls, user):
         profile_info = cls.objects.filter(user__contains=user)
 
+
+class Image(models.Model):
+    name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='images/', blank=True)
+    caption = models.TextField(blank=True)
+    posted_on = models.DateTimeField(auto_now_add=True)
+    profile = models.ForeignKey(User, blank=True, on_delete=models.CASCADE)
+    details = models.ForeignKey(Profile, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['posted_on']
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    def update_caption(self):
+        self.update()
+
+    @classmethod
+    def get_images_on_profile(cls, profile):
+        images = Image.objects.filter(profile__pk=profile)
+        return images
+
+    @property
+    def count_likes(self):
+        likes = self.likes.count()
+        return likes
+
+    @property
+    def count_comments(self):
+        comments = self.comments.count()
+        return comments
+
+
+    @classmethod
+    def all_images(cls):
+        images = cls.objects.all()
+        return images
+
+    @classmethod
+    def search_by_profile(cls, profile):
+        photo = Profile.objects.filter(name_icontains=profile)[0]
+        return cls.objects.filter(profile_id=photo.id)
+
+    def __str__(self):
+        return self.name
+
+
+class Comments(models.Model):
+    """
+    Class that contains comments details
+    """
+    image = models.ForeignKey(Image, blank=True, on_delete=models.CASCADE, related_name='comment')
+    commenter = models.ForeignKey(User, blank=True)
+    comment_itself = models.TextField()
+
+    def delete_comment(self):
+        self.delete()
+
+    def save_comment(self):
+        self.save()
+
+    @classmethod
+    def get_comments_on_image(cls, id):
+        the_comments = Comment.objects.filter(image__pk=id)
+        return the_comments
+
+    def __str__(self):
+        return self.comment_itself
+
+
+class Likes(models.Model):
+    who_liked = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
+    liked_image = models.ForeignKey(
+    Image, on_delete=models.CASCADE, related_name='likes')
+
+    def save_like(self):
+        self.save()
+
+    def __str__(self):
+        return self.who_liked
     
