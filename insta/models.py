e6a52caa40df39bf46d449a1879053357ca751c4
Balvine <balvy075@gmail.com>
2020-10-18 12:52:16 +0300

    created profile model

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+
+class Profile(models.Model):
+    """
+    Class that contains profile details
+    """
+    pic = models.ImageField(upload_to='images/', blank=True)
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE, related_name="profile")
+    bio = models.TextField()
