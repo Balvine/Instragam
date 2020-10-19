d1da1c3ad601fa7b13823e382229c3cd18a3dfbd
Balvine <balvy075@gmail.com>
2020-10-19 08:14:02 +0300

    changed settings file

--- instagram/settings.py
+++ instagram/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'insta',
     'bootstrap3'
+    'tinymce',
+    'friendship',
 ]
 
 MIDDLEWARE = [
@@ -107,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
