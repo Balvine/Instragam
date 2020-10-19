218fc1f769c3507bb4802999983c9f94e40ba598
Balvine <balvy075@gmail.com>
2020-10-19 14:10:53 +0300

    made migrations

--- insta/models.py
+++ insta/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.dispatch import receiver
-# from tinymce.models import HTMLField
+from tinymce.models import HTMLField
 from django.db.models.signals import post_save
-# from friendship.models import Friend, Follow, Block
+from friendship.models import Friend, Follow, Block
 
 
 
